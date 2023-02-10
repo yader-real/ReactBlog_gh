dbb99ac2b570cd666d8dfac9974b9a23be378be2
yader-real <yader.real@gmail.com>
2023-02-10 11:19:56 -0600

    ReactBlog v1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './Header';
+import Nav from './Nav';
+import Footer from './Footer';
+import Home from './Home';
+import NewPost from './NewPost';
+import PostPage from './PostPage';
+import EditPost from './EditPost';
+import About from './About';
+import Missing from './Missing';
+import {Route, Routes} from 'react-router-dom';
+import { useEffect } from 'react';
+import useAxiosFetch from './hooks/useAxiosFetch';
+import {useStoreActions} from 'easy-peasy';
 
 function App() {
+  const setPosts = useStoreActions((actions) => actions.setPosts);
+  
+  const {data, fetchError, isLoading} = useAxiosFetch('http://localhost:3500/posts');
+
+  useEffect(() =>{
+    setPosts(data);
+  }, [data, setPosts])
+
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <Header title="React JS Blog"  />
+        <Nav /> 
+        <Routes>
+          <Route path="/" element = {<Home 
+                                        isLoading = {isLoading}
+                                        fetchError = {fetchError}
+                                    />}/>
+          {/* <Route exact path="/">
+          <Home
+            isLoading={isLoading}
+            fetchError={fetchError}
+          />
+          </Route>                           */}
+          <Route path="/post" element = {<NewPost />}/>
+          <Route path="/edit/:id" element = {<EditPost />}/>
+          <Route path="/post/:id" element = {<PostPage />}/>
+          <Route path="/about" element = {<About />}/>
+          <Route path="*" element = {<Missing />}/>
+        </Routes>
+        <Footer />
+     </div>
   );
 }
 
